fbc38d18f6756d2dbb715cf94c9ae54e21a0af14
cpjosejesus <cp.josejesus@gmail.com>
2024-03-24 20:57:52 -0400

    Added js feature

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const buttonEdit = document.querySelector(".profile__button_edit");
+const popupProfile = document.querySelector(".popup");
+const buttonClose = document.querySelector(".popup__button_close");
+
+const profileName = document.querySelector(".profile__name");
+const profileType = document.querySelector(".profile__type");
+const profileForm = document.querySelector(".popup__form");
+const inputName = document.querySelector(".popup__item_name");
+const inputType = document.querySelector(".popup__item_type");
+
+function togglePopup(popup) {
+  inputName.value = profileName.innerHTML;
+  inputType.value = profileType.innerHTML;
+  popup.classList.toggle("popup__opened");
+}
+
+buttonEdit.addEventListener("click", function () {
+  togglePopup(popupProfile);
+});
+
+buttonClose.addEventListener("click", function () {
+  togglePopup(popupProfile);
+});
+
+profileForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileType.textContent = inputType.value;
+  togglePopup(popupProfile);
+});
