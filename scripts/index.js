a5743d8400e753268350be8460b403e01d026c22
cpjosejesus <cp.josejesus@gmail.com>
2024-03-29 13:06:08 -0400

    start sprint6

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,33 @@ const profileForm = document.querySelector(".popup__form");
 const inputName = document.querySelector(".popup__item_name");
 const inputType = document.querySelector(".popup__item_type");
 
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
 function togglePopup(popup) {
   inputName.value = profileName.innerHTML;
   inputType.value = profileType.innerHTML;
